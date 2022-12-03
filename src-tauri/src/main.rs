8f8069fedb7beb90344159189e375932e4bdb66b
Mark Devilleres <enigmaticjayar@gmail.com>
2022-12-03 16:37:48 +0800

    Add normalize.css

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,20 +1,17 @@
 #![cfg_attr(all(not(debug_assertions), target_os = "windows"), windows_subsystem = "windows")]
 
-use tauri::{WindowBuilder, WindowUrl};
+use tauri::{ WindowBuilder, WindowUrl };
 
 fn main() {
 	tauri::Builder
 		::default()
 		.setup(|app| {
-			let _main = WindowBuilder::new(
-				app,
-				"main".to_string(),
-				WindowUrl::App("/".into())
-			)
-			.title("Steel Design")
-			.maximized(true)
-			.resizable(true)
-			.build().expect("Unable to create window");
+			let _main = WindowBuilder::new(app, "main".to_string(), WindowUrl::App("/".into()))
+				.title("Steel Design")
+				.maximized(true)
+				.resizable(true)
+				.build()
+				.expect("Unable to create window");
 
 			Ok(())
 		})
