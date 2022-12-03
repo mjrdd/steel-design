dc75b300fb6863f30ddbce3ce39e1f8d564cd80d
mjrdd <mddevilleres@gmail.com>
2022-12-03 20:37:38 +0800

    Add AISC Shapes Database

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,7 +1,10 @@
+/** @type {import('prettier').Config} */
 module.exports = {
 	useTabs: true,
 	trailingComma: "none",
 	printWidth: 100,
+	bracketSameLine: true,
+	endOfLine: "crlf",
 	plugins: [require("prettier-plugin-svelte"), require("prettier-plugin-rust")],
 	pluginSearchDirs: ["."],
 	overrides: [
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,166 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import { onMount } from "svelte";
+
+	interface AISCShapes {
+		Type: string;
+		EDI_Std_Nomenclature: string;
+		AISC_Manual_Label: string;
+		T_F: string;
+		W: number;
+		A: number;
+		d: number;
+		ddet: number;
+		Ht: number | string;
+		h: number | string;
+		OD: number | string;
+		bf: number;
+		bfdet: number;
+		B: number | string;
+		b: number | string;
+		ID: number | string;
+		tw: number;
+		twdet: number;
+		"twdet/2": number;
+		tf: number;
+		tfdet: number;
+		t: number | string;
+		tnom: number | string;
+		tdes: number | string;
+		kdes: number;
+		kdet: number;
+		k1: number;
+		x: number | string;
+		y: number | string;
+		eo: number | string;
+		xp: number | string;
+		yp: number | string;
+		"bf/2tf": number;
+		"b/t": number;
+		"b/tdes": number | string;
+		"h/tw": number;
+		"h/tdes": number | string;
+		"D/t": number | string;
+		Ix: number;
+		Zx: number;
+		Sx: number;
+		rx: number;
+		Iy: number;
+		Zy: number;
+		Sy: number;
+		ry: number;
+		Iz: number | string;
+		rz: number | string;
+		Sz: number | string;
+		J: number;
+		Cw: number;
+		C: number | string;
+		Wno: number;
+		Sw1: number;
+		Sw2: number | string;
+		Sw3: number | string;
+		Qf: number;
+		Qw: number;
+		ro: number | string;
+		H: number | string;
+		"tan(α)": number | string;
+		Iw: number | string;
+		zA: number | string;
+		zB: number | string;
+		zC: number | string;
+		wA: number | string;
+		wB: number | string;
+		wC: number | string;
+		SwA: number | string;
+		SwB: number | string;
+		SwC: number | string;
+		SzA: number | string;
+		SzB: number | string;
+		SzC: number | string;
+		rts: number;
+		ho: number;
+		PA: number;
+		PA2: number | string;
+		PB: number;
+		PC: number;
+		PD: number;
+		T: number;
+		WGi: number;
+		WGo: number | string;
+	}
+
+	let aiscShapesEnglish: AISCShapes[] | undefined;
+	let aiscShapesMetric: AISCShapes[] | undefined;
+
+	onMount(() => {
+		import("$lib/data/aisc-shapes-english.json").then((res) => {
+			aiscShapesEnglish = res.default;
+		});
+
+		import("$lib/data/aisc-shapes-metric.json").then((res) => {
+			aiscShapesMetric = res.default;
+		});
+	});
+
+	let activeUnits: "english" | "metric" = "english";
+
+	function getActiveAiscShapeData(units: typeof activeUnits, shape: string) {
+		if (!(aiscShapesEnglish && aiscShapesMetric)) return;
+
+		return (units === "english" ? aiscShapesEnglish : aiscShapesMetric).find(
+			(item) => item["EDI_Std_Nomenclature"] === shape
+		);
+	}
+
+	$: activeAiscShape = activeUnits === "english" ? "W44X335" : "W1100X499";
+	$: activeAiscShapeData = getActiveAiscShapeData(activeUnits, activeAiscShape);
+
+	$: console.log(activeAiscShapeData);
+</script>
+
+<div class="window">
+	<div>
+		<div class="section">
+			<h3>Units</h3>
+
+			<input
+				type="radio"
+				name="units"
+				id="units-english"
+				bind:group={activeUnits}
+				value="english" />
+			<label for="units-english">English</label>
+
+			<input type="radio" name="units" id="units-metric" bind:group={activeUnits} value="metric" />
+			<label for="units-metric">Metric</label>
+		</div>
+
+		{#if aiscShapesEnglish && aiscShapesMetric}
+			<select bind:value={activeAiscShape}>
+				{#if activeUnits === "english"}
+					{#each aiscShapesEnglish as shape}
+						<option value={shape["EDI_Std_Nomenclature"]}>{shape["EDI_Std_Nomenclature"]}</option>
+					{/each}
+				{:else}
+					{#each aiscShapesMetric as shape}
+						<option value={shape["EDI_Std_Nomenclature"]}>{shape["EDI_Std_Nomenclature"]}</option>
+					{/each}
+				{/if}
+			</select>
+		{:else}
+			<p>Loading AISC Shapes data...</p>
+		{/if}
+	</div>
+</div>
+
+<style>
+	.window {
+		min-width: 100vw;
+		min-height: 100vh;
+		padding: 16px;
+		display: grid;
+	}
+
+	.section {
+		margin: 16px 0;
+	}
+</style>
