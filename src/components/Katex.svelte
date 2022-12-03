905bedf82e7d147dd1875e186016a2f630d0b9d6
Mark Devilleres <enigmaticjayar@gmail.com>
2022-12-03 16:41:59 +0800

    Add katex

---
+++ src/components/Katex.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	import katex from "katex";
+
+	export let render = "";
+	export let displayMode = false;
+</script>
+
+{@html katex.renderToString(render, { displayMode, throwOnError: false })}
