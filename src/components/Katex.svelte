909113627b974bf58b79ea81fae6dec96794ba23
mjrdd <mddevilleres@gmail.com>
2022-12-12 20:22:06 +0800

    throwOnError in katex during dev environment

--- src/components/Katex.svelte
+++ src/components/Katex.svelte
@@ -1,8 +1,9 @@
 <script lang="ts">
 	import katex from "katex";
+	import { dev } from "$app/environment";
 
 	export let render = "";
 	export let displayMode = false;
 </script>
 
-{@html katex.renderToString(render, { displayMode, throwOnError: false })}
+{@html katex.renderToString(render, { displayMode, throwOnError: dev })}
